bf984ad69eb8c7a6b7fe1191e48e08883e18be9a
murali1821 <muralikannan666@gmail.com>
2023-03-24 11:43:05 +0530

    hello

---
+++ src/org/samejava/SampleJava.java
@@ -0,0 +1,15 @@
+package org.samejava;
+
+public class SampleJava {
+	
+	private void methodName() {
+		
+		System.out.println("qwertyuioasdfghjkzxcvbn");
+		
+	}
+
+	public static void main(String[] args) {
+		SampleJava j=new SampleJava();
+		j.methodName();
+	}
+}
---
+++ src/org/samejava/SamplePgm.java
@@ -0,0 +1,20 @@
+package org.samejava;
+
+public class SamplePgm {
+	
+
+		public void studentName(){
+			System.out.println("Student Name is : Murali K ");
+		}
+		public void studentId(){
+		System.out.println("Student id is : 51965157");
+		}
+
+		public static void main(String[] args){
+		SamplePgm a = new SamplePgm();
+		a.studentName();
+		a.studentId();
+		}
+	}
+
+
